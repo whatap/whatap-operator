cb3f5a251e15acdba0d16f981fef82763b79b3a3
jykim <jykim@whatap.io>
2025-05-16 10:28:34 +0900

    v0.3.3-mu03:add mutating admission webhook for automatic Whatap APM injection into Pods

--- internal/webhook/v2alpha1/whatapagent_webhook.go
+++ internal/webhook/v2alpha1/whatapagent_webhook.go
@@ -20,6 +20,8 @@ import (
 	"context"
 	"fmt"
 	corev1 "k8s.io/api/core/v1"
+	"os"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -32,7 +34,7 @@ import (
 
 // nolint:unused
 // log is for logging in this package.
-var whatapagentlog = logf.Log.WithName("whatapagent-resource")
+var whatapWebhookLogger = logf.Log.WithName("whatap-webhook")
 
 // SetupWhatapAgentWebhookWithManager registers the webhook for WhatapAgent in the manager.
 func SetupWhatapAgentWebhookWithManager(mgr ctrl.Manager) error {
@@ -47,7 +49,7 @@ func SetupWhatapAgentWebhookWithManager(mgr ctrl.Manager) error {
 // TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
 type WhatapAgentCustomDefaulter struct {
-	// TODO(user): Add more fields as needed for defaulting
+	client client.Client // webhook 에 등록된 mgr.GetClient()
 }
 
 var _ webhook.CustomDefaulter = &WhatapAgentCustomDefaulter{}
@@ -55,6 +57,32 @@ var _ webhook.CustomDefaulter = &WhatapAgentCustomDefaulter{}
 // Default implements webhook.CustomDefaulter so a webhook will be registered for the Kind WhatapAgent.
 func (d *WhatapAgentCustomDefaulter) Default(ctx context.Context, obj runtime.Object) error {
 	pod, ok := obj.(*corev1.Pod)
+
+	// WhatapAgent CR 가져오기 (클러스터 스코프)
+	var whatapAgentCustomResource monitoringv2alpha1.WhatapAgent
+	if err := d.client.Get(ctx, client.ObjectKey{Name: "whatap"}, &whatapAgentCustomResource); err != nil {
+		// CR이 아직 생성 안 됐으면 주입 안 함
+		return nil
+	}
+	defaultNS := os.Getenv("WHATAP_DEFAULT_NAMESPACE")
+	if defaultNS == "" {
+		defaultNS = "whatap-monitoring"
+	}
+	ns := whatapAgentCustomResource.Spec.Features.KubernetesMonitoring.KubernetesMonitoringNamespace
+	if ns == "" {
+		ns = defaultNS
+	}
+
+	for _, target := range whatapAgentCustomResource.Spec.Features.Apm.Instrumentation.Targets {
+		if target.Enabled != "true" {
+			continue
+		}
+		if !hasLabels(pod.Labels, target.PodSelector.MatchLabels) {
+			continue
+		}
+		// controller 패치 함수 재사용
+		patchPodTemplateSpec(&pod.Spec, whatapAgentCustomResource, target, whatapWebhookLogger)
+	}
 	if !ok {
 		return fmt.Errorf("expected a Pod but got a %T", obj)
 	}
@@ -64,7 +92,7 @@ func (d *WhatapAgentCustomDefaulter) Default(ctx context.Context, obj runtime.Ob
 	}
 	// TODO(user): fill in your defaulting logic.
 	pod.Annotations["mutating-admission-webhook"] = "whatap"
-	whatapagentlog.Info("Annotated Pod", "name", pod.Name)
+	whatapWebhookLogger.Info("Annotated Pod", "name", pod.ObjectMeta.Name)
 	return nil
 }
 
@@ -80,7 +108,7 @@ func (v *WhatapAgentCustomValidator) ValidateCreate(ctx context.Context, obj run
 	if !ok {
 		return nil, fmt.Errorf("expected a WhatapAgent object but got %T", obj)
 	}
-	whatapagentlog.Info("Validation for WhatapAgent upon creation", "name", whatapagent.GetName())
+	whatapWebhookLogger.Info("Validation for WhatapAgent upon creation", "name", whatapagent.GetName())
 
 	// TODO(user): fill in your validation logic upon object creation.
 
@@ -93,7 +121,7 @@ func (v *WhatapAgentCustomValidator) ValidateUpdate(ctx context.Context, oldObj,
 	if !ok {
 		return nil, fmt.Errorf("expected a WhatapAgent object for the newObj but got %T", newObj)
 	}
-	whatapagentlog.Info("Validation for WhatapAgent upon update", "name", whatapagent.GetName())
+	whatapWebhookLogger.Info("Validation for WhatapAgent upon update", "name", whatapagent.GetName())
 
 	// TODO(user): fill in your validation logic upon object update.
 
@@ -106,7 +134,7 @@ func (v *WhatapAgentCustomValidator) ValidateDelete(ctx context.Context, obj run
 	if !ok {
 		return nil, fmt.Errorf("expected a WhatapAgent object but got %T", obj)
 	}
-	whatapagentlog.Info("Validation for WhatapAgent upon deletion", "name", whatapagent.GetName())
+	whatapWebhookLogger.Info("Validation for WhatapAgent upon deletion", "name", whatapagent.GetName())
 
 	// TODO(user): fill in your validation logic upon object deletion.
 
